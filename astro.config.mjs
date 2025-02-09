5851181228df73b1308aa3937da9980437030be1
Soumyo Dey <heysoumyo@gmail.com>
2025-02-09 17:20:55 +0530

    Configure for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from "@astrojs/node";
 
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'hybrid',
-  adapter: node({
-    mode: 'standalone'
-  }),
   site: 'https://ace139.github.io',
+  base: '/',
+  integrations: [tailwind()],
+  output: 'static',
 });
