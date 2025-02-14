799eebb56148d68801081f537c8976b2fccdfb59
Soumyo Dey <heysoumyo@gmail.com>
2025-02-14 23:58:08 +0530

    BugFix: Social Icons Rendering

--- src/config/socials.ts
+++ src/config/socials.ts
@@ -1,33 +1,28 @@
 interface Social {
   platform: string;
   url: string;
-  icon: string;
   label: string;
 }
 
 export const socials: Social[] = [
   {
-    platform: 'X',
+    platform: 'x-twitter',
     url: 'https://x.com/heySoumyo',
-    icon: 'fab fa-x-twitter',
     label: 'X'
   },
   {
     platform: 'linkedin',
     url: 'https://linkedin.com/in/soumyo-dey',
-    icon: 'fab fa-linkedin',
     label: 'LinkedIn'
   },
   {
     platform: 'github',
     url: 'https://github.com/ace139',
-    icon: 'fab fa-github',
     label: 'GitHub'
   },
   {
     platform: 'email',
     url: 'mailto:heysoumyo@gmail.com',
-    icon: 'fas fa-envelope',
     label: 'Email'
   },
 ];
